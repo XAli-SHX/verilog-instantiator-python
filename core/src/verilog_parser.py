48e32f3b1c2b77a611c601656563bda010bd2ee0
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-07-12 09:17:19 +0330

    Fix bug in parsing and finding output port

--- core/src/verilog_parser.py
+++ core/src/verilog_parser.py
@@ -5,16 +5,32 @@ def parse_verilog_module(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
 
-    # Regular expressions to match input and output port declarations
-    input_regex = r'input\s+(\[\d+:\d+\]\s+)?\w+\s*;'
-    output_regex = r'output\s+(\[\d+:\d+\]\s+)?\w+\s*;'
+    content = content.replace('\n', ' ')
 
-    # Find input ports
-    input_ports = re.findall(input_regex, content)
-    input_ports = [re.search(r'\w+', port).group() for port in input_ports]
+    # Regular expression to match module declaration and port list
+    module_regex = r'module\s+(\w+)\s*\((.*?)\);'
 
-    # Find output ports
-    output_ports = re.findall(output_regex, content)
-    output_ports = [re.search(r'\w+', port).group() for port in output_ports]
+    # Find module declaration and port list
+    module_match = re.search(module_regex, content, re.DOTALL)
+    if not module_match:
+        raise ValueError("Failed to find module declaration")
+
+    module_name = module_match.group(1)
+    port_list = module_match.group(2)
+
+    # Regular expression to match individual ports
+    port_regex = r'\s*(input|output|inout)?\s*(\[\d+:\d+\])?\s*(\w+)\s*(?:|\))'
+
+    # Find input and output ports
+    ports = re.findall(port_regex, port_list)
+
+    input_ports = []
+    output_ports = []
+    for port in ports:
+        port_type, port_range, port_name = port
+        if 'input' in port_type:
+            input_ports.append(port_name)
+        if 'output' in port_type:
+            output_ports.append(port_name)
 
     return input_ports, output_ports
