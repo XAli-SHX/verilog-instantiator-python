d356f110db75c2acc2da65ad4b252d6d3ea91c88
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-07-12 08:51:05 +0330

    Add parser

---
+++ core/src/verilog_parser.py
@@ -0,0 +1,28 @@
+import re
+
+
+def parse_verilog_module(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    # Regular expressions to match input and output port declarations
+    input_regex = r'input\s+(\[\d+:\d+\]\s+)?\w+\s*;'
+    output_regex = r'output\s+(\[\d+:\d+\]\s+)?\w+\s*;'
+
+    # Find input ports
+    input_ports = re.findall(input_regex, content)
+    input_ports = [re.search(r'\w+', port).group() for port in input_ports]
+
+    # Find output ports
+    output_ports = re.findall(output_regex, content)
+    output_ports = [re.search(r'\w+', port).group() for port in output_ports]
+
+    return input_ports, output_ports
+
+
+# Usage example
+module_file = 'example_module.v'
+inputs, outputs = parse_verilog_module(module_file)
+
+print('Input ports:', inputs)
+print('Output ports:', outputs)
