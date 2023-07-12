4a428961fd0552eb4640a95d02000d1189ad55cb
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-07-12 08:55:40 +0330

    Move Verilog test files to verilog dir

--- core/src/main.py
+++ core/src/main.py
@@ -2,7 +2,7 @@ import verilog_parser
 
 
 def main():
-    inputs, outputs = verilog_parser.parse_verilog_module('../test/test.v')
+    inputs, outputs = verilog_parser.parse_verilog_module('../test/verilog/test.v')
     print('Input ports:', inputs)
     print('Output ports:', outputs)
 
