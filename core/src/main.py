772f4d9c90f8cbaaa8c9a96b76632a79da76873d
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-07-12 09:22:57 +0330

    Test parsing for test.v

--- core/src/main.py
+++ core/src/main.py
@@ -3,8 +3,8 @@ import verilog_parser
 
 def main():
     inputs, outputs = verilog_parser.parse_verilog_module('../test/verilog/test.v')
-    print('Input ports:', inputs)
-    print('Output ports:', outputs)
+    print('Input ports:', [i.name for i in inputs])
+    print('Output ports:', [o.name for o in outputs])
 
 
 if __name__ == '__main__':
